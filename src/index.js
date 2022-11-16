e83afd87460eb0191d0f1fe08746084fd25c0a62
meowkameow <81650663+meowkameow@users.noreply.github.com>
2022-11-16 14:16:11 +0300

    lesson_3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './lesson_2/App';
+import App from './lesson_3/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
